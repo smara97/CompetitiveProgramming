f3933985c6210ef4145916a87591fda164caba8c
smara97 <35504456+smara97@users.noreply.github.com>
2018-03-06 12:07:29 +0200

    Create CodeChef MARCHA1.cpp

---
+++ Code chef/CodeChef MARCHA1.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARINGS
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<vector>
+#include<set>
+#include<cmath>
+#include<bitset>
+#include<iterator>
+#include<queue>
+#include<unordered_map>
+#include<iomanip>
+#include<functional>
+#include<stack>
+#include<sstream>
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define PI 3.14159265;
+typedef long long ll;
+using namespace std;
+int main() {
+	ll n;
+	int tc;
+	cin >> tc;
+	while (tc--) {
+		ll n, m; bool ok = 0;
+		cin >> n >> m;
+		vector<ll>bn(n);
+		for (int i = 0; i < n; i++)
+			cin >> bn[i];
+		for (int i = 0; i < (1 << n); i++) {
+			ll sum = 0;
+			for (int j = 0; j < n; j++) {
+				sum += (bool(i&(1 << j))*bn[j]);
+			}
+			if (sum == m) {
+				cout << "Yes\n"; ok = 1;
+				break;
+			}
+		}
+		if (!ok)cout << "No\n";
+	}
+	return 0;
+}

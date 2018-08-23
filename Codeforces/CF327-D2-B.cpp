0c5036519f96b3cc162d24016cf6ffb2761c826c
Ahmed Mohamed Sayed <35504456+smara97@users.noreply.github.com>
2018-08-23 13:06:42 +0200

    Create CF327-D2-B.cpp

---
+++ Codeforces/CF327-D2-B.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARINGS
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define PI 3.14159265;
+#define endl '\n'
+typedef long long ll;
+using namespace std;
+void File() {
+	freopen("", "r", stdin);
+	freopen("", "w", stdout);
+}
+long double srt(ll x,ll y) {
+	return sqrt(pow(x, 2) + pow(y, 2));
+}
+ int main() {
+   fast;
+   int n;
+   cin >> n;
+   int ind = 1e5;
+   while (n--)cout << ind++ << ' ';
+   cout << endl;
+		return 0;
+}

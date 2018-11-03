9dd222594c9bb406baa9035b997c96ee431de4f4
Ahmed Mohamed Sayed <35504456+smara97@users.noreply.github.com>
2018-11-03 16:49:56 +0200

    Create CF-97C - Replacement.cpp

---
+++ Codeforces/CF-97C - Replacement.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+#define m3lsh return 0;
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+ll gcd(ll a, ll b) { return !b ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+int  dix[] = { 1, -1, 0, 0, 1, 1, -1, -1 };
+int  diy[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int main() {
+	fast;
+	int n; cin >> n;
+	vector<ll>v(n);
+	for (int i = 0; i < n; i++)cin >> v[i];
+	sort(v.begin(), v.end());
+	if (v.back() == 1)v[n - 1] = 2;
+	else v.push_back(1);
+	sort(v.begin(), v.end());
+	for (int i = 0; i < n; i++)cout << v[i] << ' '; cout << endl;
+	m3lsh;
+}

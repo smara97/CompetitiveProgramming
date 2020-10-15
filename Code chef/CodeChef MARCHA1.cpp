c2a3f492103e45d4b337fbc6924e5fb2be2d9954
Alokpurbey <72925197+AlokPurbey@users.noreply.github.com>
2020-10-15 18:29:50 +0530

    Update CodeChef MARCHA1.cpp

--- Code chef/CodeChef MARCHA1.cpp
+++ Code chef/CodeChef MARCHA1.cpp
@@ -1,19 +1,5 @@
 #define _CRT_SECURE_NO_WARINGS
-#include<iostream>
-#include<algorithm>
-#include<string>
-#include<map>
-#include<vector>
-#include<set>
-#include<cmath>
-#include<bitset>
-#include<iterator>
-#include<queue>
-#include<unordered_map>
-#include<iomanip>
-#include<functional>
-#include<stack>
-#include<sstream>
+#include<bits/stdc++.h>
 #define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define PI 3.14159265;
 typedef long long ll;
